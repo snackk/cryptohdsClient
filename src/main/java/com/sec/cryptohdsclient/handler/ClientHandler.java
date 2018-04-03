bf21d6cb1a7e6833d882d6d49b3ba194fab483c5
Unknown <santos_matrix@sapo.pt>
2018-04-03 01:37:47 +0100

    Fixed package. Implemented REST calls. Trying to use library funcs.

--- src/main/java/com/sec/cryptohdsclient/handler/ClientHandler.java
+++ src/main/java/com/sec/cryptohdsclient/handler/ClientHandler.java
@@ -1,10 +1,30 @@
 package com.sec.cryptohdsclient.handler;
 
+import com.sec.cryptohdsclient.web.rest.LedgerResource;
+import com.sec.cryptohdsclient.web.rest.SecurityResource;
+import com.sec.cryptohdslibrary.envelope.Envelope;
 import com.sec.cryptohdslibrary.keystore.KeyStoreImpl;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class ClientHandler {
 
     private KeyStoreImpl clientKeyStore;
+
+    private final LedgerResource ledgerResource;
+
+    private final SecurityResource securityResource;
+
+    private String cryptoServerPublicKey;
+
+    public ClientHandler(LedgerResource ledgerResource, SecurityResource securityResource) {
+        this.ledgerResource = ledgerResource;
+        this.securityResource = securityResource;
+
+        this.cryptoServerPublicKey = this.securityResource.getPublicKey();
+    }
 
     private KeyStoreImpl getKeyStore() {
         return clientKeyStore;
@@ -12,5 +32,9 @@ public class ClientHandler {
 
     public void register(String ledgerName, String ledgerPassword) {
         this.clientKeyStore = new KeyStoreImpl(ledgerName, ledgerPassword);
+
+        Envelope envelope = new Envelope(ledgerName, this.clientKeyStore);
+        System.out.println(envelope.getCipheredEnvelope(cryptoServerPublicKey));
+        this.ledgerResource.createLedger(envelope.getCipheredEnvelope(cryptoServerPublicKey));
     }
 }
