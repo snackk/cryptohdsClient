276343dbfa0f7e3c8572e63f42941340e9b2ccd1
Unknown <santos_matrix@sapo.pt>
2018-04-05 02:00:20 +0100

    Custom Rest Exception handling.

--- src/main/java/com/sec/cryptohdsclient/web/rest/OperationResource.java
+++ src/main/java/com/sec/cryptohdsclient/web/rest/OperationResource.java
@@ -1,5 +1,6 @@
 package com.sec.cryptohdsclient.web.rest;
 
+import com.sec.cryptohdsclient.web.rest.exceptions.CryptohdsRestException;
 import com.sec.cryptohdslibrary.envelope.Envelope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +10,26 @@ import org.springframework.stereotype.Service;
 public class OperationResource extends CryptohdsResource {
 
     public boolean sendOperation(Envelope envelope, String publicKey) {
-        ResponseEntity<Envelope> result = secureRequest(envelope, "operation/send", publicKey);
+        try {
+            ResponseEntity<Envelope> result = secureRequest(envelope, "operation/send", publicKey);
 
-        return result.getStatusCode() == HttpStatus.NO_CONTENT;
+            return result.getStatusCode() == HttpStatus.NO_CONTENT;
+
+        } catch (CryptohdsRestException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
     }
 
     public boolean receiveOperation(Envelope envelope, String publicKey) {
-        ResponseEntity<Envelope> result = secureRequest(envelope, "operation/receive", publicKey);
+        try {
+            ResponseEntity<Envelope> result = secureRequest(envelope, "operation/receive", publicKey);
 
-        return result.getStatusCode() == HttpStatus.NO_CONTENT;
+            return result.getStatusCode() == HttpStatus.NO_CONTENT;
+
+        } catch (CryptohdsRestException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
     }
 }
---
+++ src/main/java/com/sec/cryptohdsclient/web/rest/errors/CustomRestExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.sec.cryptohdsclient.web.rest.errors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sec.cryptohdsclient.web.rest.exceptions.CryptohdsRestException;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.ResponseErrorHandler;
+
+import java.io.IOException;
+
+public class CustomRestExceptionHandler implements ResponseErrorHandler {
+
+    private ResponseErrorHandler errorHandler = new DefaultResponseErrorHandler();
+
+    public boolean hasError(ClientHttpResponse response) throws IOException {
+        return errorHandler.hasError(response);
+    }
+
+    public void handleError(ClientHttpResponse response) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        ApiError result = mapper.readValue(response.getBody(), ApiError.class);
+        CryptohdsRestException exception = new CryptohdsRestException(result.getMessage());
+
+        throw exception;
+    }
+}
