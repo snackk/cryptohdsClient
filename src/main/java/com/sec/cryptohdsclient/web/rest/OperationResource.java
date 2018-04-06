d4a8b09d2de76b906c11a8191248e7bbcca5bbb7
Unknown <santos_matrix@sapo.pt>
2018-04-06 01:21:14 +0100

    Sequence number done.

--- src/main/java/com/sec/cryptohdsclient/web/rest/OperationResource.java
+++ src/main/java/com/sec/cryptohdsclient/web/rest/OperationResource.java
@@ -9,27 +9,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class OperationResource extends CryptohdsResource {
 
-    public boolean sendOperation(Envelope envelope, String publicKey) {
-        try {
-            ResponseEntity<Envelope> result = secureRequest(envelope, "operation/send", publicKey);
+    public boolean sendOperation(Envelope envelope, String publicKey) throws CryptohdsRestException {
+        ResponseEntity<Envelope> result = secureRequest(envelope, "operation/send", publicKey);
 
-            return result.getStatusCode() == HttpStatus.NO_CONTENT;
-
-        } catch (CryptohdsRestException e) {
-            System.out.println(e.getMessage());
-        }
-        return false;
+        return result.getStatusCode() == HttpStatus.NO_CONTENT;
     }
 
-    public boolean receiveOperation(Envelope envelope, String publicKey) {
-        try {
-            ResponseEntity<Envelope> result = secureRequest(envelope, "operation/receive", publicKey);
+    public boolean receiveOperation(Envelope envelope, String publicKey) throws CryptohdsRestException {
+        ResponseEntity<Envelope> result = secureRequest(envelope, "operation/receive", publicKey);
 
-            return result.getStatusCode() == HttpStatus.NO_CONTENT;
-
-        } catch (CryptohdsRestException e) {
-            System.out.println(e.getMessage());
-        }
-        return false;
+        return result.getStatusCode() == HttpStatus.NO_CONTENT;
     }
 }
