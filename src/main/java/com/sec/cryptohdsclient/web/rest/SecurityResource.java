c3cdab697ffa0538bce6ba96192f51b015dc9d40
Unknown <santos_matrix@sapo.pt>
2018-04-25 18:37:51 +0100

    Enabled passing IP's and Port by arguments & properties

--- src/main/java/com/sec/cryptohdsclient/web/rest/SecurityResource.java
+++ src/main/java/com/sec/cryptohdsclient/web/rest/SecurityResource.java
@@ -1,23 +1,30 @@
 package com.sec.cryptohdsclient.web.rest;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class SecurityResource {
 
-    @Value("${rest.ip}")
-    private static String restIp;
+    private String restIp;
 
-    @Value("${rest.port}")
-    private static int restPort;
+    private int restPort;
 
-    private static final String URL = "http://localhost:8080/api/";
+    public String URL;
+
+    private final Environment environment;
+
+    public SecurityResource(Environment environment) {
+        this.environment = environment;
+
+        this.restPort = Integer.parseInt(this.environment.getProperty("rest.port"));
+        this.restIp = this.environment.getProperty("rest.ip");
+        this.URL = "http://" + restIp + ":" + restPort + "/api/";
+    }
 
     public String getPublicKey() {
         RestTemplate restTemplate = new RestTemplate();
-
         return restTemplate.getForObject(URL + "security/keys", String.class);
     }
 }
