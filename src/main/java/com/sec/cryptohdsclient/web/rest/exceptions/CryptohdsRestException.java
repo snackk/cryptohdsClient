865a51ff060fefd71d1e063404137ceca1af6fbc
André Bernardino <11179175+anbernar@users.noreply.github.com>
2018-05-03 17:55:10 +0100

    change artifact id and add custom serial number to exception.

--- src/main/java/com/sec/cryptohdsclient/web/rest/exceptions/CryptohdsRestException.java
+++ src/main/java/com/sec/cryptohdsclient/web/rest/exceptions/CryptohdsRestException.java
@@ -2,7 +2,9 @@ package com.sec.cryptohdsclient.web.rest.exceptions;
 
 public class CryptohdsRestException extends RuntimeException {
 
-    private String message;
+	private static final long serialVersionUID = -541274631102609914L;
+
+	private String message;
 
     public CryptohdsRestException(String message) {
         this.message = message;
