b470f48fed287c35983bca2390eed5982ef3afa9
Unknown <santos_matrix@sapo.pt>
2018-05-10 01:09:00 +0100

    closes #1

--- src/main/java/com/sec/cryptohdsclient/web/rest/SecurityResource.java
+++ src/main/java/com/sec/cryptohdsclient/web/rest/SecurityResource.java
@@ -1,9 +1,11 @@
 package com.sec.cryptohdsclient.web.rest;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -21,8 +23,12 @@ public class SecurityResource {
     public HashMap<String,String> getPublicKey() {
         RestTemplate restTemplate = new RestTemplate();
         HashMap<String,String> ipKeysMap = new HashMap<>();
-        for(String ip : ips) {
-        	ipKeysMap.put(ip,restTemplate.getForObject(ip + "security/keys", String.class));
+        for(String ip : ips) {//InvocationTargetException
+            try {
+                ipKeysMap.put(ip, restTemplate.getForObject(ip + "security/keys", String.class));
+            } catch (RestClientException e) {
+                // Communication failure with ip
+            }
         }
         return ipKeysMap;
     }
